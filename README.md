58a6e1951112a8611304488a5f8c45d77602c448
Darren Jensen <darren.jensen@gmail.com>
2024-12-24 17:32:13 +0700

    converted to cli tool using commander

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Notion API Wrapper
 
 A simple API Wrapper for Notion based on [this tutorial](https://www.youtube.com/watch?v=QlUt06XLbJE).
+
+## Install
+
+## Run
+
+Run using `npm`:
+
+```bash
+npm run cli getPage
+```
+
+Run using `node` and `jq` (to get pretty formatted JSON in the terminal!)
+
+```bash
+node index.js getPage | jq
+```
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 require('dotenv').config();
+const { Command } = require('commander');
 const NotionWrapper = require('./modules/notion');
-
 const { NOTION_PAGE_ID } = require('./modules/constants');
 
-const getPage = async () => {
-    const notionWrapper = new NotionWrapper(NOTION_PAGE_ID);
-    const response = await notionWrapper.getPage();
-    // console.log(response)
-}
+const program = new Command();
+const notionWrapper = new NotionWrapper(NOTION_PAGE_ID);
 
-const main = async () => {
-    await getPage();
-}
+program
+  .name('notion-cli')
+  .description('CLI to interact with Notion API')
+  .version('1.0.0');
 
-main();
+program
+  .command('getPage')
+  .description('Retrieve a page from Notion')
+  .action(async () => {
+    try {
+      const response = await notionWrapper.getPage();
+      console.log(JSON.stringify(response, null, 2));
+    } catch (error) {
+      console.error('Error retrieving page:', error);
+    }
+  });
+
+program.parse(process.argv);
