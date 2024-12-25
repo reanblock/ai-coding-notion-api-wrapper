f52eed2d6e18b9caba8725b15a56dc6fc3997d49
Darren Jensen <darren.jensen@gmail.com>
2024-12-25 10:43:06 +0700

    added comments to wrapper

--- modules/notion.js
+++ modules/notion.js
@@ -1,6 +1,13 @@
 const { Client } = require("@notionhq/client");
 
+/**
+ * A wrapper class for interacting with the Notion API.
+ */
 class NotionWrapper {
+    /**
+     * Initializes the NotionWrapper with a page ID.
+     * @param {string} pageId - The ID of the Notion page.
+     */
     constructor(pageId) {
         this.notion = new Client({
             auth: process.env.NOTION_TOKEN,
@@ -8,10 +15,18 @@ class NotionWrapper {
         this.pageId = pageId;
     }
 
+    /**
+     * Retrieves the page details from Notion.
+     * @returns {Promise<Object>} The page details.
+     */
     async getPage() {
         const response = await this.notion.pages.retrieve({ page_id: this.pageId });
         return response;
     }
+    /**
+     * Retrieves all blocks from the specified page.
+     * @returns {Promise<Array>} An array of blocks.
+     */
     async getPageBlocks() {
         const blocks = [];
         let cursor;
@@ -26,6 +41,12 @@ class NotionWrapper {
         return blocks;
     }
 
+    /**
+     * Adds a text block to the specified parent block.
+     * @param {string} text - The text content to add.
+     * @param {string} [parentBlock=this.pageId] - The parent block ID.
+     * @returns {Promise<Object>} The response from the Notion API.
+     */
     async addText(text, parentBlock = this.pageId) {
         const response = await this.notion.blocks.children.append({
             block_id: parentBlock,
@@ -48,6 +69,11 @@ class NotionWrapper {
         });
         return response;
     }
+    /**
+     * Deletes a block by its ID.
+     * @param {string} blockId - The ID of the block to delete.
+     * @returns {Promise<Object>} The response from the Notion API.
+     */
     async deleteBlock(blockId) {
         const response = await this.notion.blocks.delete({
             block_id: blockId,
